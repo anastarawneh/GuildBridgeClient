a65c29e434291285aa7e9b8da7fdba497b4a2b54
anastarawneh <anastarawneh2001@icloud.com>
2022-11-20 00:04:47 +0300

    WS message types + Send version & UUID to connect

--- src/main/java/me/anastarawneh/guildbridgeclient/websocket/WebSocketInstance.java
+++ src/main/java/me/anastarawneh/guildbridgeclient/websocket/WebSocketInstance.java
@@ -3,6 +3,7 @@ package me.anastarawneh.guildbridgeclient.websocket;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.mojang.realmsclient.gui.ChatFormatting;
+import me.anastarawneh.guildbridgeclient.GuildBridgeClient;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.common.ForgeHooks;
@@ -25,13 +26,32 @@ public class WebSocketInstance extends WebSocketClient {
     @Override
     public void onMessage(String message) {
         JsonObject json = new JsonParser().parse(message).getAsJsonObject();
-        String username = json.get("username").getAsString();
-        String content = json.get("message").getAsString()
-                .replaceAll("<gbcm>", String.valueOf(ChatFormatting.AQUA))
-                .replaceAll("</gbcm>", String.valueOf(ChatFormatting.RESET));
-                ChatFormatting.BLUE + "Discord > " + ChatFormatting.GOLD + username + ChatFormatting.RESET + ": " + content
-        )));
+        switch (json.get("status").getAsInt()) {
+            case 0:
+                // Send data
+                send("{\"uuid\": \"" + Minecraft.getMinecraft().getSession().getProfile().getId() + "\", \"version\": \"" + GuildBridgeClient.VERSION + "\"}");
+                break;
+            case 1:
+                // Accepted
+                break;
+            case 2:
+                // User message incoming
+                String username = json.getAsJsonObject("data").get("username").getAsString();
+                String content = json.getAsJsonObject("data").get("message").getAsString()
+                        .replaceAll("<gbcm>", String.valueOf(ChatFormatting.AQUA))
+                        .replaceAll("</gbcm>", String.valueOf(ChatFormatting.RESET))
+                        .replaceAll("<gbcrm>", String.valueOf(ChatFormatting.AQUA))
+                        .replaceAll("</gbcrm>", String.valueOf(ChatFormatting.RESET))
+                        .replaceAll("<gbccm>", String.valueOf(ChatFormatting.AQUA))
+                        .replaceAll("</gbccm>", String.valueOf(ChatFormatting.RESET));
                 Minecraft.getMinecraft().thePlayer.addChatMessage((ForgeHooks.newChatWithLinks(
+                        ChatFormatting.BLUE + "Discord > " + ChatFormatting.GOLD + username + ChatFormatting.RESET + ": " + content
+                )));
+                break;
+            default:
+                GuildBridgeClient.LOGGER.error("Received bad status code.");
+                break;
+        }
     }
 
     @Override
